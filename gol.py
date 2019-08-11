5a320f1e39e001eabcaf2ebb681de3f69aad9929
Severi Nättilä <severi.nattila@gmail.com>
2019-08-11 09:09:35 +0300

    add glider_gun

--- gol.py
+++ gol.py
@@ -5,29 +5,43 @@ import matplotlib.animation as animation
 import random
 import argparse
 
-def makeGrid(cols,rows,theme):
+def makeGrid(cols,rows,gridtype):
     world = np.zeros((cols,rows))
 
-    if theme == 1:
+    if gridtype == 0:
         for i in range(0,cols):
             for j in range(0,rows):
                 world[i,j] = random.randint(0,1)
 
-    elif theme == 2:
+    elif gridtype == 1:
         beacon = [[1, 1, 0, 0],
                   [1, 1, 0, 0],
                   [0, 0, 1, 1],
                   [0, 0, 1, 1]]
-                  
+
         world[1:5, 1:5] = beacon
 
-    elif theme == 3:  
+    elif gridtype== 2:  
         glider = [[0, 1, 0, 0],
                   [0, 0, 1, 0],
                   [1, 1, 1, 0],
                   [0, 0, 0, 0]]
 
-        world[1:5, 1:5] = glider
+        world[1:5, 1:5] = glider          
+
+    elif gridtype == 3:
+        glider_gun = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+                      [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0],
+                      [0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1],
+                      [0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1],
+                      [1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                      [1,1,0,0,0,0,0,0,0,0,1,0,0,0,1,0,1,1,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0],
+                      [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+                      [0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                      [0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
+                
+        world[1:10, 1:37] = glider_gun
+
 
     return(world)
 
@@ -59,27 +73,28 @@ def animate(world):
     plt.axis('on')
     ims = []
     i = 0
-    rotation = 100
+    rotation = 200
 
     for n in range(rotation):
         ims.append((plt.imshow(world, cmap='binary'),))
         world = gen(world)
         i+=1
         print(i,'/',rotation)
-    im_ani = animation.ArtistAnimation(fig, ims, interval=100,
+
+    im_ani = animation.ArtistAnimation(fig, ims, interval=60,
     repeat_delay=10000, blit=True)
     plt.show()
 
 
 if __name__ == '__main__':
-    cols = 40
-    rows = 40
-    theme = 1
+    cols = 50
+    rows = 50
+    gridtype = 3
 
     #parses
     #parser = argparse.ArgumentParser(description="Conway's Game of Life")
     #parser.add_argument('--grid-size cols', dest='cols', required=False)
     #parser.add_argument('--grid-size rows', dest='rows', required=False)
 
-    world = makeGrid(cols,rows,theme)
+    world = makeGrid(cols,rows,gridtype)
     animate(world)
